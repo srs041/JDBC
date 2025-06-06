4920f90d87181dbbbd3774f47c76578a3c12bf5b
Seetal <gudufb1@gmail.com>
2025-06-06 16:15:16 +0530

    Inserted data to table through JDBC

--- src/main/java/org/jdbc/InsertData.java
+++ src/main/java/org/jdbc/InsertData.java
@@ -3,7 +3,6 @@ package org.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.Statement;
 
 public class InsertData {
     public static void main(String[] args) {
