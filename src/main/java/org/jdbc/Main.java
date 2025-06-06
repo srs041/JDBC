7d3c49e28190128798c404ceec87da793264a776
Seetal Ranjan Sahoo <31173539+srs041@users.noreply.github.com>
2025-06-06 15:33:25 +0530

    first jdbc project

---
+++ src/main/java/org/jdbc/Main.java
@@ -0,0 +1,29 @@
+package org.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/employeedb";
+        String username = "root";
+        String password = "Csgo1234@";
+        try {
+            //load the driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //Create connection
+            Connection con = DriverManager.getConnection(url, username, password);
+            if (con.isClosed()) {
+                System.out.println("Connection Closed");
+            } else {
+                System.out.println("Connection opened");
+            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
